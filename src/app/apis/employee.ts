dbac7e1f9e268fd9d24fd83595263db196965ce6
Devanshi Patel <devanshipatel2824@gmail.com>
2025-12-29 18:03:22 +0530

    Concept of http-client

---
+++ src/app/apis/employee.ts
@@ -0,0 +1,36 @@
+import { inject, Injectable } from '@angular/core';
+import { form } from '@angular/forms/signals';
+import { Form } from '../form/form';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Emp {
+  id: string | null;
+  name: string | null;
+  email: string | null;
+  gender: string | null;
+  phone: string | null;
+  salary: number | null;
+  city: string | null;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Employee {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:3000/employee';
+  getEmployee(): Observable<Emp[]> {
+    return this.http.get<Emp[]>(`${this.apiUrl}`);
+  }
+  createEmployee(emp: Emp): Observable<Emp> {
+    return this.http.post<Emp>(this.apiUrl, emp);
+  }
+  updateEmployee(id: string, emp: Emp) {
+    return this.http.put(`${this.apiUrl}/${id}`, emp);
+  }
+  deleteEmployee(id: string) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+}
--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Emp, Employee } from '../apis/employee';
 interface Employeeform {
+  id: FormControl<string | null>;
   name: FormControl<string | null>;
   email: FormControl<string | null>;
   gender: FormControl<string | null>;
@@ -11,26 +13,46 @@ interface Employeeform {
 }
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule,FormsModule],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
 export class Form {
+
+  private employees = inject(Employee);
   form = new FormGroup<Employeeform>({
+    id: new FormControl(null, Validators.required),
     name: new FormControl(null, Validators.required),
     email: new FormControl(null,
       [Validators.required,
-      Validators.pattern('/^[a-zA-Z0-9._%+-]+@tag97(\.[a-zA-Z]{2,})?$/')
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@tag97(\.[a-zA-Z]{2,})?$/)
       ]),
     gender: new FormControl(null, Validators.required),
     phone: new FormControl(null, [Validators.required, Validators.pattern('^[6-9]\\d{9}$')]),
-    salary: new FormControl(null, [Validators.required,Validators.min(1000),Validators.max(10000)]),
+    salary: new FormControl(null, [Validators.required, Validators.min(1000), Validators.max(10000)]),
     city: new FormControl(null, Validators.required)
   })
+ onSubmit() {
+  const id = this.form.value.id;
 
-  onSubmit() {
-    console.log(this.form.value);
+  if (id) {
+    // ✅ UPDATE existing record
+    this.employees.updateEmployee(id, this.form.value as Emp)
+      .subscribe(() => {
+        alert('Record JSON server ma UPDATE thai gayo');
+        this.form.reset();
+      });
+  } else {
+    // ✅ ADD new record
+    this.employees.createEmployee(this.form.value as Emp)
+      .subscribe(() => {
+        alert('Employee Added');
+        this.form.reset();
+      });
+
   }
+  
+}
+
 
 }
-// Validators.pattern(/^[a-zA-Z0-9._%+-]+@tag97(\.[a-zA-Z]{2,})?$/)
